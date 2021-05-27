de1684bfa2c3d7818b6502d50bd93030d06aa3f9
Ali Rahimi <alrhim@amazon.com>
2021-05-26 21:38:21 -0700

    Solve constrained form optimization ||X^-1 A - I||

--- inv.py
+++ inv.py
@@ -4,24 +4,31 @@ import torch
 
 # Solve
 #   min_X || X^-1 A - I ||
-# by gradient descent. The optimum is reached when X = A.
+# by gradient descent when X is a 5x5 matrix with zeros everywhere
+# except the diagonal and the upper right and lower left entries.
 
 class Invert(torch.nn.Module):
     def __init__(self, X: torch.Tensor):
         super().__init__()
         self.X = torch.nn.Parameter(X)
 
+        
     def forward(self, A: torch.Tensor):
-        assert A.shape == self.X.shape
+        assert A.shape == (5, 5)
 
-        return torch.sum((torch.linalg.solve(self.X, A) - torch.eye(*A.shape))**2)
+        X_full = torch.sparse_coo_tensor(
+            [[0, 1, 2, 3, 4, 0, 4], [0, 1, 2, 3, 4, 4, 0]], self.X, (5,5)
+        ).to_dense() + torch.eye(5, 5)
+
+        return torch.sum((torch.linalg.solve(X_full, A) - torch.eye(*A.shape)) ** 2)
+
 
 def main():
     A = torch.rand(5, 5, dtype=torch.float64)
     A += torch.eye(*A.shape)
 
-    model = Invert(torch.eye(*A.shape, dtype=torch.double))
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
+    model = Invert(torch.ones(A.shape[0] + 2, dtype=torch.double))
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1)
 
     losses = np.zeros(1000)
     for it in range(losses.size):
@@ -36,12 +43,10 @@ def main():
     print("A =\n", A)
     print("X =\n", model.X)
     print("loss(X) = ", float(model(A)))
-
-    Xhat = np.linalg.inv(A)
-    print("Xhat =\n", Xhat)
-    print("loss(Xhat) = 0")
+    print("dloss/dX = ", model.X.grad)
 
     plt.plot(losses)
     plt.show()
+
 
 main()
